e970ad398ea578e38965c5cefe3e34d316ad73b7
Shabbar Adamjee <adamjeeshabbar@gmail.com>
2023-11-11 10:30:01 +0530

    All done

--- Final_Login_Dialog.py
+++ Final_Login_Dialog.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import pymysql
 
 class login_window(ctk.CTk):
     def __init__(self, width, height):
@@ -12,6 +13,7 @@ class login_window(ctk.CTk):
         self.overrideredirect(True)
         self.attributes('-topmost', True)
 
+        self.bind('<Return>', frame(None).establish_connection)
         self.bind('<Escape>', lambda e: self.destroy())
 
 
@@ -30,8 +32,24 @@ class frame(ctk.CTkFrame):
         login_label = ctk.CTkLabel(self, text="Login", font=("Helvetica", 20, "bold"), text_color="white") 
 
         # Entry widgets
-        self.username_entry = ctk.CTkEntry(self, placeholder_text="username", placeholder_text_color="grey", fg_color="transparent", border_color="#0492C2")
-        self.pswd_entry = ctk.CTkEntry(self, placeholder_text="password", placeholder_text_color="grey", fg_color="transparent", border_color="#0492C2", show="🖕🏻")
+        self.username_var = ctk.StringVar()
+        self.pwd_var = ctk.StringVar()
+
+        self.username_entry = ctk.CTkEntry(self,
+                                           placeholder_text="username", 
+                                           placeholder_text_color="grey", 
+                                           fg_color="transparent", 
+                                           border_color="#0492C2", 
+                                           textvariable=self.username_var)
+        
+        self.pswd_entry = ctk.CTkEntry(self, 
+                                       placeholder_text="password", 
+                                       placeholder_text_color="grey", 
+                                       fg_color="transparent", 
+                                       border_color="#0492C2", 
+                                       show="🖕🏻", 
+                                       textvariable=self.pwd_var)
+        
         self.user_entry_rel_width = 0.7
         self.pwd_entry_rel_width = 0.7
         
@@ -42,20 +60,25 @@ class frame(ctk.CTkFrame):
         self.pswd_entry.bind('<FocusOut>', self.deanimate_pwd_entry)
 
         # Error labels
-        user_error = ctk.CTkLabel(self, text="", text_color="red", font=("Helvetica", 15))
-        pswd_error = ctk.CTkLabel(self, text="", text_color="red", font=("Helvetica", 15))
+        self.error = ctk.CTkLabel(self, text="", text_color="red", font=("Helvetica", 15))
 
         # Enter button
-        submit = ctk.CTkButton(self, text="Submit", border_color="#0492C2",corner_radius=20,  font=("Helvetica", 15), text_color="white", command=lambda e: self.login())
+        submit = ctk.CTkButton(self, 
+                               text="Submit", 
+                               border_color="#0492C2",
+                               corner_radius=20,
+                               font=("Helvetica", 15), 
+                               text_color="white", 
+                               command=self.establish_connection)
 
         # Placing Widgets
         login_label.place(relx=0.5, rely=0.1, anchor='center')
         self.username_entry.place(relx=0.5, rely=0.4,relheight = 0.1, relwidth=self.user_entry_rel_width, anchor='center')
         self.pswd_entry.place(relx=0.5, rely=0.6,relheight = 0.1, relwidth=self.pwd_entry_rel_width, anchor='center')
-        submit.place(relx=0.5, rely=0.82, anchor='center', relwidth=0.4)
+        submit.place(relx=0.5, rely=0.85, anchor='center', relwidth=0.4)
 
-        user_error.place(relx= 0.5, rely=0.5, anchor='center')
-        pswd_error.place(relx= 0.7, rely=0.5, anchor='center')
+        self.error.place(relx= 0.5, rely=0.77, anchor='s')
+        # pswd_error.place(relx= 0.7, rely=0.5, anchor='center')
 
     def animate_user_entry(self, event):
         self.user_entry_rel_width += 0.016
@@ -81,25 +104,35 @@ class frame(ctk.CTkFrame):
             self.pswd_entry.place(relx=0.5, rely=0.6, relheight = 0.1, relwidth=self.pwd_entry_rel_width, anchor='center')
             home.after(1, lambda: self.deanimate_pwd_entry(event))
 
-    def login(self, uname, pswd ):
-        print(f'Username: {uname}\nPassword: {pswd}\n')
-        if uname != self.username:
-                self.username_entry.delete(0, 'end')
-                self.pswd_entry.delete(0, 'end')
-                
-                self.user_error.configure(text="User does not exist")
-        
-        elif uname == self.username:
-                self.user_error.configure(text="User Found", text_color="green") 
-                if pswd != self.password:
-                    self.pswd_entry.delete(0, 'end')
-                    self.pswd_error.configure(text="Incorrect Password")
-                else:
-                    self.pswd_error.configure(text="Password matched! Signing in", text_color="green")
+    def establish_connection(self):
+        host = 'localhost'
+        user = self.username_var.get()
+        pwd = self.pwd_var.get()
+        database = 'empproject'
+
+        try:
+            connection = pymysql.connect(host=host, user=user, password=pwd, database=database)
+        except pymysql.err.OperationalError:
+            self.error.configure(text='')
+            self.error.configure(text='Username/Password Invalid')
+            self.username_entry.configure(border_color='red')
+            self.pswd_entry.configure(border_color='red')
+            print('Failure')
         else:
-                self.pswd_entry.delete(0, 'end')
-                self.pswd_error.configure(text="")
-                self.user_error.configure(text="")
+            self.error.configure(text='')
+            print('Success')
+            self.error.configure(text_color='green')
+            self.error.configure(text='Login Successful')
+            home.after(1500, home.destroy)
+
+        return connection
+        
 
 home = login_window(300, 300)
+
+conneecion = None
+
+# connection = frame(None).establish_connection()
+# cursor = connection.cursor()
+
 home.mainloop()
